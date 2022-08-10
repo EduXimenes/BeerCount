9c74584ef65be263b7edb0cb852aa6aea864ee8a
EduXimenes <e.ximenes17@hotmail.com>
2022-08-10 11:09:17 -0300

    Initial Commit

---
+++ BeerCount/BeerCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerCountApp.swift
+//  BeerCount
+//
+//  Created by Eduardo Ximenes on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BeerCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
